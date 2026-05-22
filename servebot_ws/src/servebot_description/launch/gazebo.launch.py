685b9d0a3b8a038d807f7d9e9eb00e83175c7955
Nirbhay Tejani <nirbhaytejani1234@gmail.com>
2026-05-22 17:07:07 +0530

    Add files via upload

---
+++ servebot_ws/src/servebot_description/launch/gazebo.launch.py
@@ -0,0 +1,61 @@
+import os
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
+from launch.substitutions import Command, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    servebot_description = get_package_share_directory("servebot_description")
+
+    model_arg = DeclareLaunchArgument(
+        name="model", default_value=os.path.join(
+                servebot_description, "urdf", "servebot.urdf.xacro"
+            ),
+        description="Absolute path to robot urdf file"
+    )
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
+                                        value_type=str)
+                                        
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{"robot_description": robot_description}]
+    )
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name="GZ_SIM_RESOURCE_PATH",
+        value=[
+            str(Path(servebot_description).parent.resolve())])
+    
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
+                launch_arguments=[
+                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
+                    )
+                ]
+             )
+    
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=["-topic", "robot_description",
+                   "-name", "servebot"],
+    )
+
+    return LaunchDescription([
+        model_arg,
+        gazebo_resource_path,
+        robot_state_publisher_node,
+        gazebo,
+        gz_spawn_entity,
+        
+    ])
